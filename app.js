7317d4b91585e23e9afd78b8e3984caff974c18a
de4ev <decev.v@gmail.com>
2018-08-31 23:24:38 +0300

    routes

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const analyticsRoutes = require('./routes/analytics');
+const categoryRoutes = require('./routes/category');
+const positionRoutes = require('./routes/position');
+const orderRoutes = require('./routes/order');
+const app = express();
+const keys = require('./config/keys')
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected.'))
+    .catch((error) => console.log(error))
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api/auth', authRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/position', positionRoutes);
+app.use('/api/order', orderRoutes);
+
+module.exports = app;
