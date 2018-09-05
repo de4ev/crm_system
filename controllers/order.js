41c4b86e14b0bb27a98e5bd20075d1845637cd0e
de4ev <decev.v@gmail.com>
2018-09-05 23:17:32 +0300

    orders controller

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,58 @@
-module.exports.getAll = function(req, res) {
+const Order = require('../models/Order')
+const errorHandler = require('../utils/errorHandler')
+
+
+// GET localhost:5000/api/order?offset=2&limit=5
+module.exports.getAll = async function(req, res) {
+    
+    const query = {
+        user: req.user.id
+    }
+
+    if (req.query.start) {
+        query.date = {
+            $gte: req.query.start
+        }
+    }
+
+    if (req.query.end) {
+        if (!req.query.start) {
+            query.date = {}
+        }
+        query.date['$lte'] = req.query.end
+    }
+
+    if (req.query.order) {
+        query.order = +req.query.order
+    }
+
+    try {
+        const orders = await Order
+            .find(query)
+            .sort({date: -1})
+            .skip(+req.query.offset)
+            .limit(+req.query.limit)
+
+        res.status(200).json(orders)
+    } catch(e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.create = function(req, res) {
+module.exports.create = async function(req, res) {
+    try {
+        const lastOrder = Order
+            .findOne({user: req.user.id})
+            .sort({date: -1})
+
+        const maxOrder = lastOrder ? lastOrder : 0
+        const order = await new Order({
+            order: maxOrder + 1,
+            list: req.body.list,
+            user: req.user.id
+        }).save()
+        res.status(200).json(order)
+    } catch(e) {
+        errorHandler(res, e)
+    }
 }
