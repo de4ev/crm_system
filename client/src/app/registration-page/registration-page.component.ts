8834b5e8f7e2fd13667b098f50cb7f36d8f010cc
de4ev <decev.v@gmail.com>
2018-09-24 23:58:41 +0300

    auth service (register)

--- client/src/app/registration-page/registration-page.component.ts
+++ client/src/app/registration-page/registration-page.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
   styleUrls: ['./registration-page.component.css']
 })
-export class RegistrationPageComponent implements OnInit {
+export class RegistrationPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  form: FormGroup
+  sub: Subscription
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+  onSubmit() {
+    this.form.disable()
+    this.sub = this.auth.register(this.form.value).subscribe(
+      () => {
+        this.router.navigate(['/login'], {
+          queryParams: {
+            registered: true
+          }
+        })
+      },
+      (error) => {
+        console.warn(error)
+        this.form.enable()
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe()
+    }
   }
 
 }
