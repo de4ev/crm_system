4d7983b3497219e59e86f6522891d339816d0c92
de4ev <decev.v@gmail.com>
2018-09-30 23:49:17 +0300

    keep token on refresh

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>\n',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(
+    private auth: AuthService
+  ) {}
+  ngOnInit() {
+    const potentialToken = localStorage.getItem('authToken');
+    if (potentialToken != null) {
+      this.auth.setToken(potentialToken)
+    }
+  }
 }
