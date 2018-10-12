c17bf4dd01b0a8e93946f84e02ed3ff1ef8c69a0
de4ev <decev.v@gmail.com>
2018-10-12 23:56:52 +0300

    categories form validation

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categories-form',
@@ -8,16 +9,26 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class CategoriesFormComponent implements OnInit {
 
+  form: FormGroup
   isNew = true
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required)
+    })
+
+
     this.route.params.subscribe((params: Params) => {
       if (params['id']) {
         this.isNew = false
       }
     })
+  }
+
+  onSubmit() {
+    
   }
 
 }
