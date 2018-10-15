e6d44d1bf71c8454e888a21ba8bb21530ad96e97
de4ev <decev.v@gmail.com>
2018-10-15 12:17:53 +0300

    category creating/editing done

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -5,6 +5,7 @@ import { CategoriesService } from '../../shared/services/categories.service';
 import { switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { MaterialService } from '../../shared/classes/material.service';
+import { Category } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-categories-form',
@@ -18,9 +19,11 @@ export class CategoriesFormComponent implements OnInit {
   image: File
   form: FormGroup
   isNew = true
+  imagePreview = ''
+  category: Category
 
   constructor(private route: ActivatedRoute,
-              private categoriesService: CategoriesService) { }
+    private categoriesService: CategoriesService) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -28,24 +31,26 @@ export class CategoriesFormComponent implements OnInit {
     })
 
     this.form.disable()
-    
+
     this.route.params
       .pipe(
-         switchMap((params: Params) => {
+        switchMap((params: Params) => {
           if (params['id']) {
             this.isNew = false
             return this.categoriesService.getById(params['id'])
-          } 
+          }
           return of(null)
-         }
+        }
         )
       )
       .subscribe(
-        category => {
+        (category: Category) => {
           if (category) {
+            this.category = category
             this.form.patchValue({
               name: category.name
             })
+            this.imagePreview = category.imageSrc
             MaterialService.updateTextInputs()
           }
           this.form.enable()
@@ -62,12 +67,34 @@ export class CategoriesFormComponent implements OnInit {
     const file = event.target.files[0]
     this.image = file
 
-    // const reader = new FileReader()
-    // reader.onload()
+    const reader = new FileReader()
+    reader.onload = () => {
+      this.imagePreview = reader.result
+    }
+    reader.readAsDataURL(file)
   }
 
   onSubmit() {
-    
-  }
+    let obs$
 
+    this.form.disable()
+
+    if (this.isNew) {
+      obs$ = this.categoriesService.create(this.form.value.name, this.image)
+    } else {
+      obs$ = this.categoriesService.update(this.category._id, this.form.value.name, this.image)
+    }
+
+    obs$.subscribe(
+      (category) => {
+        this.category = category
+        MaterialService.toast('Изменения сохранены.')
+        this.form.enable()
+      },
+      (error) => {
+        MaterialService.toast(error.error.message)
+        this.form.enable()
+      }
+    )
+  }
 }
