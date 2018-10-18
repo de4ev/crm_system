7eb2895ae772cf5999f94ef5e31b4dd3d9cc0096
de4ev <voldemort_001@mail.ru>
2018-10-18 23:33:50 +0300

    position interface

--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -1,15 +1,22 @@
 export interface User {
-    email: string
-    password: string
+    email: string;
+    password: string;
 }
 
 export interface Message {
-    message: string
+    message: string;
 }
 
 export interface Category {
-    name: string
-    imageSrc?: string
-    user?: string
-    _id?: string
+    name: string;
+    imageSrc?: string;
+    user?: string;
+    _id?: string;
+}
+export interface Position {
+    name: string;
+    cost: number;
+    category: string;
+    user?: string;
+    _id?: string;
 }
