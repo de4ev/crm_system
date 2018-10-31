47776eb8c0f0e0e275ffc046a29517ab40fcb3d6
de4ev <decev.v@gmail.com>
2018-10-31 23:58:39 +0300

    migrating from mLab to atlas

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,10 @@
 module.exports = {
-    mongoURI: 'mongodb://de4ev:1qaz2wsx@ds139992.mlab.com:39992/crm_db',
+    mongoURI: 'mongodb+srv://de4ev:1qaz2wsx@cluster0-sac8k.mongodb.net/test?retryWrites=true',
     jwt: 'dev-jwt'
 }
+
+
+// module.exports = {
+//     mongoURI: 'mongodb://de4ev:1qaz2wsx@ds139992.mlab.com:39992/crm_db',
+//     jwt: 'dev-jwt'
+// }
