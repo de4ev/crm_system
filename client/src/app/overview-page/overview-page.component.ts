74995f5b3114de3b67ece026100a8b0391437cd3
de4ev <decev.v@gmail.com>
2018-11-17 19:38:09 +0300

    overview-page service

--- client/src/app/overview-page/overview-page.component.ts
+++ client/src/app/overview-page/overview-page.component.ts
@@ -1,4 +1,7 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from '../shared/services/analytics.service';
+import { OverviewPage } from '../shared/interfaces';
 
 @Component({
   selector: 'app-overview-page',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewPageComponent implements OnInit {
 
-  constructor() { }
+  data$: Observable<OverviewPage>
+
+  constructor(
+    private service: AnalyticsService
+  ) { }
 
   ngOnInit() {
+    this.data$ = this.service.getOverview()
   }
 
 }
